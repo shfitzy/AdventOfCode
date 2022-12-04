2bfeb6b3fc3a329921e30e3ced9a0170267e0bb6
Shane <shfitzy@gmail.com>
2022-12-04 15:35:23 -0600

    I lied, needed to update one last variable name.

--- projects/2022/Day1/solution.py
+++ projects/2022/Day1/solution.py
@@ -4,7 +4,7 @@ file_path = os.path.dirname(os.path.realpath(__file__))
 
 def read_input_and_transform(filename):
     with open(filename) as f:
-        return sorted([sum(list(map(lambda i: int(i), row.split('\n')))) for row in f.read().split('\n\n')], reverse=True)
+        return sorted([sum(list(map(lambda i: int(i), group.split('\n')))) for group in f.read().split('\n\n')], reverse=True)
 
 def calcTotalCalories(elf_calorie_totals, top=1):
     return sum(elf_calorie_totals[:top])
