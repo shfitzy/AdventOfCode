2fc83cb005a34259fd4004fd194692b0bab64604
Shane <shfitzy@gmail.com>
2022-12-04 15:30:36 -0600

    Moar cleanup! itertools no longer needed.

--- projects/2022/Day1/solution.py
+++ projects/2022/Day1/solution.py
@@ -1,14 +1,13 @@
 import os
-import itertools
 
 file_path = os.path.dirname(os.path.realpath(__file__))
 
 def read_lines(filename):
     with open(filename) as f:
-        return f.read().splitlines()
+        return f.read().split('\n\n')
 
 def transform_input(input):
-    return sorted([sum(list(map(lambda i: int(i), g))) for m, g in itertools.groupby(input, key=lambda x: x != '') if m], reverse=True)
+    return sorted([sum(list(map(lambda i: int(i), row.split('\n')))) for row in input], reverse=True)
 
 def calcTotalCalories(elf_calorie_totals, top=1):
     return sum(elf_calorie_totals[:top])
