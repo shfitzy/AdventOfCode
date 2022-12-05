6fe794fffe456403c33bb6db18aaa9c72634a342
Shane <shfitzy@gmail.com>
2022-12-04 18:03:27 -0600

    Removing the dictionary object since it's not necessary. Calculating the score based on indices now.

--- projects/2022/Day2/solution.py
+++ projects/2022/Day2/solution.py
@@ -2,28 +2,26 @@ import os
 
 file_path = os.path.dirname(os.path.realpath(__file__))
 
-MOVES = {'A': {'beats': 'C', 'score': 1}, 'B': {'beats': 'A', 'score': 2}, 'C': {'beats': 'B', 'score': 3}}
-
 def read_input_and_transform(filename):
     with open(filename) as f:
         return f.read().splitlines()
 
 def get_result_score(my_move, opponent_move):
-    if MOVES[my_move] == MOVES[opponent_move]: return 3
-    elif MOVES[my_move]['beats'] == opponent_move: return 6
+    if my_move == opponent_move: return 3
+    elif my_move == (opponent_move + 1) % 3: return 6
     else: return 0
 
 def get_game_score(my_move, opponent_move):
-    return MOVES[my_move]['score'] + get_result_score(my_move, opponent_move)
+    return (my_move + 1) + get_result_score(my_move, opponent_move)
 
 def part_1(line):
-    return chr(ord(line.split()[1]) - ord('X') + ord('A'))
+    return ord(line.split()[1]) - ord('X')
 
 def part_2(line):
-    return chr((ord(line.split()[0]) - ord('A') + ['X', 'Y', 'Z'].index(line.split()[1]) - 1) % 3 + ord('A'))
+    return (['A', 'B', 'C'].index(line.split()[0]) + (['X', 'Y', 'Z'].index(line.split()[1]) - 1)) % 3
 
 def play_games(input, algo=part_1):
-    return sum([get_game_score(algo(line), line.split()[0]) for line in input])
+    return sum([get_game_score(algo(line), ord(line.split()[0]) - ord('A')) for line in input])
 
 if __name__ == '__main__':
     input = read_input_and_transform(file_path + os.path.sep + 'input.txt')
