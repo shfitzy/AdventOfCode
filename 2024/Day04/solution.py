ee17efd88799680349dbc17d3ac80600d31a3ced
Shane <shfitzy@gmail.com>
2024-12-04 00:20:51 -0600

    Refactored part 2 of the solution

--- 2024/Day04/solution.py
+++ 2024/Day04/solution.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import os
 import sys
 
@@ -7,48 +8,50 @@ sys.path.append(os.path.join(file_path, os.path.pardir, os.path.pardir))
 
 from utils import file_util
 
+def xmas(data):
+    count = 0
+
+    for i in range(len(data)):
+            for j in range(len(data[i])):
+                if data[i][j] == 'X':
+                    if i >= 3:
+                        if data[i-1][j] == 'M' and data[i-2][j] == 'A' and data[i-3][j] == 'S':
+                            count += 1
+                    if i >= 3 and j >= 3:
+                        if data[i-1][j-1] == 'M' and data[i-2][j-2] == 'A' and data[i-3][j-3] == 'S':
+                            count += 1
+                    if j >= 3:
+                        if data[i][j-1] == 'M' and data[i][j-2] == 'A' and data[i][j-3] == 'S':
+                            count += 1
+                    if i <= len(data) - 4 and j >= 3:
+                        if data[i+1][j-1] == 'M' and data[i+2][j-2] == 'A' and data[i+3][j-3] == 'S':
+                            count += 1
+                    if i <= len(data) - 4:
+                        if data[i+1][j] == 'M' and data[i+2][j] == 'A' and data[i+3][j] == 'S':
+                            count += 1
+                    if i <= len(data) - 4 and j <= len(data[i]) - 4:
+                        if data[i+1][j+1] == 'M' and data[i+2][j+2] == 'A' and data[i+3][j+3] == 'S':
+                            count += 1
+                    if j <= len(data[i]) - 4:
+                        if data[i][j+1] == 'M' and data[i][j+2] == 'A' and data[i][j+3] == 'S':
+                            count += 1
+                    if i >= 3 and j <= len(data[i]) - 4:
+                        if data[i-1][j+1] == 'M' and data[i-2][j+2] == 'A' and data[i-3][j+3] == 'S':
+                            count += 1
+
+    return count
+
+def x_mas(data):
+    return sum((data[i][j] == 'A') and ({data[i-1][j-1], data[i+1][j+1]} == {data[i-1][j+1], data[i+1][j-1]} == {'M', 'S'}) 
+        for i in range(1, len(data) - 1)
+        for j in range(1, len(data[i]) - 1)
+    )
+
 if __name__ == '__main__':
-    data = file_util.read_file(file_path, 'input.txt')
-    array = [[c for c in line] for line in data]
+    data = [[c for c in line] for line in file_util.read_file(file_path, 'input.txt')]
+    data2 = defaultdict(list)
+    print(data2)
 
-    count_1 = 0
-    count_2 = 0
-    
-    for i in range(len(array)):
-        for j in range(len(array[i])):
-            if array[i][j] == 'X':
-                if i >= 3:
-                    if array[i-1][j] == 'M' and array[i-2][j] == 'A' and array[i-3][j] == 'S':
-                        count_1 += 1
-                if i >= 3 and j >= 3:
-                    if array[i-1][j-1] == 'M' and array[i-2][j-2] == 'A' and array[i-3][j-3] == 'S':
-                        count_1 += 1
-                if j >= 3:
-                    if array[i][j-1] == 'M' and array[i][j-2] == 'A' and array[i][j-3] == 'S':
-                        count_1 += 1
-                if i <= len(array) - 4 and j >= 3:
-                    if array[i+1][j-1] == 'M' and array[i+2][j-2] == 'A' and array[i+3][j-3] == 'S':
-                        count_1 += 1
-                if i <= len(array) - 4:
-                    if array[i+1][j] == 'M' and array[i+2][j] == 'A' and array[i+3][j] == 'S':
-                        count_1 += 1
-                if i <= len(array) - 4 and j <= len(array[i]) - 4:
-                    if array[i+1][j+1] == 'M' and array[i+2][j+2] == 'A' and array[i+3][j+3] == 'S':
-                        count_1 += 1
-                if j <= len(array[i]) - 4:
-                    if array[i][j+1] == 'M' and array[i][j+2] == 'A' and array[i][j+3] == 'S':
-                        count_1 += 1
-                if i >= 3 and j <= len(array[i]) - 4:
-                    if array[i-1][j+1] == 'M' and array[i-2][j+2] == 'A' and array[i-3][j+3] == 'S':
-                        count_1 += 1
-
-    for i in range(len(array) - 2):
-        for j in range(len(array[i]) - 2):
-            if array[i+1][j+1] == 'A':
-                x_1 = array[i][j] + array[i+2][j+2]
-                x_2 = array[i+2][j] + array[i][j+2]
-                if x_1 in {'MS', 'SM'} and x_2 in {'MS', 'SM'}:
-                    count_2 += 1
-
-    print(count_1)
-    print(count_2)
+    print(xmas(data))
+    print(x_mas(data))
+    print(x_mas_2(data))
