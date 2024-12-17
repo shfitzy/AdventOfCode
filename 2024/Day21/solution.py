beaae363906e47b027460c18868e9911c91c0031
Shane <shfitzy@gmail.com>
2024-12-16 21:07:14 -0600

    Adding folders for the rest of the days

---
+++ 2024/Day21/solution.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+file_path = os.path.dirname(os.path.realpath(__file__))
+
+sys.path.append(os.path.join(file_path, os.path.pardir, os.path.pardir))
+
+from utils import file_util
+
+if __name__ == '__main__':
+    input = file_util.read(file_path, 'input.txt')
