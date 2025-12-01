6e4b53de62eef01f7f71c327ac54af461bf97c9d
Shane <shfitzy@gmail.com>
2025-12-01 00:09:33 -0600

    Refactors

--- 2025/Day01/solution.py
+++ 2025/Day01/solution.py
@@ -7,32 +7,21 @@ sys.path.append(os.path.join(file_path, os.path.pardir, os.path.pardir))
 
 from utils import file_util
 
+def process_input(input):
+    p1, p2, dial_value = 0, 0, 50
+    
+    dir = 'R'
+    for i in input:
+        new_dir, rotation = i[0], int(i[1:])
+        if new_dir != dir:
+            dial_value = 100 - dial_value
+            dir = new_dir
+        
+        dial_value = (dial_value % 100) + rotation
+        if dial_value % 100 == 0: p1 += 1
+        p2 += dial_value // 100
+
+    return p1, p2
+
 if __name__ == '__main__':
-    input = file_util.read(file_path, 'input.txt').split('\n')
-
-    value = 50
-    password = 0
-    for i in input:
-        dir = 1 if i[0] == 'R' else -1
-        num = int(i[1:])
-        value = (value + dir * num) % 100
-        if value == 0: password += 1
-
-    print('Part 1: ', password)
-
-    value = 50
-    password = 0
-    for i in input:
-        dir = 1 if i[0] == 'R' else -1
-        num = int(i[1:])
-        prev_value = value
-        value += + dir * num
-        if value == 0 or value == 100: password += 1
-        if value < 0:
-            password += value * -1 // 100 + 1
-            if prev_value % 100 == 0: password -= 1
-        if value > 100: password += value // 100
-        value = value % 100
-        print(value)
-
-    print('Part 2: ', password)
+    [print(f'Part {i+1}: {x}') for i, x in enumerate(process_input(file_util.read(file_path, 'input.txt').split('\n')))]
